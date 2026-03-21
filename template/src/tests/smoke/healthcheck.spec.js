a465f91628146dd557272a523fba9ace4362f62e
Swiss Hart <49976512+swiss2487@users.noreply.github.com>
2026-03-21 19:02:15 +0000

    Update healthcheck.spec.js
    same again

--- template/src/tests/smoke/healthcheck.spec.js
+++ template/src/tests/smoke/healthcheck.spec.js
@@ -1,5 +1,5 @@
-const { test, expect } = require('@playwright/test');
-
+//const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 test('@dev @test @prod Healthcheck: add 2 numbers', async () => {
     const sum = 1 + 1;
     expect(sum).toBe(2);
