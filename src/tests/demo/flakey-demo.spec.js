24f16a421e2e8471a8f436d52253d189d82d54bb
k2261098 <k2261098@kcl.com>
2026-04-02 16:39:24 +0100

    fixing pw scipts in package.json

--- src/tests/demo/flakey-demo.spec.js
+++ src/tests/demo/flakey-demo.spec.js
@@ -2,22 +2,22 @@
 import { test, expect } from '@playwright/test';
 test.describe.configure({ retries: 2 });
 
-test('1-intentionally flaky test (PoC)', async () => {
+test('@flaky Scenario: 1-intentionally flaky test (PoC)', async () => {
   if (Math.random() < 0.5) {
     throw new Error('Random flake for PoC');
   }
 });
 
 
-test('2-tight timeout flake (PoC)', async ({ page }) => {
+test('@flaky Scenario: 2-tight timeout flake (PoC)', async ({ page }) => {
   await page.goto('https://keatsuat.kcl.ac.uk/', { timeout: 500 });
 });
 
 
-test('3-retry-based flake (PoC)', async () => {
+test('@flaky Scenario: 3-retry-based flake (PoC)', async () => {
   expect(Math.random()).toBeGreaterThan(0.7);
 });
 
-test('4-intentionally slow test (PoC)', async () => {
+test('@flaky Scenario: 4-intentionally slow test (PoC)', async () => {
   await new Promise(r => global.setTimeout(r, 2000));
 });
